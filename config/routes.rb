ecad028918333f70b6de13be22dbe3df5dfa81fa
makena <roselynemakena@gmail.com>
2017-06-20 08:27:01 +0000

    Add views and structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'welcome#home'
+
+
+
+
+
+
+
+
+
+  devise_for :user, skip: [:sessions]
+  as :user do
+  	get 'login', to: 'devise/sessions#new'
+  	get 'logout', to: 'devise/sessions#destroy'
+
+
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
